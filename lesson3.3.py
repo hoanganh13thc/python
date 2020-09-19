0ea467aa9a9c068b5871329e98c017cd60c90b7d
hoanganh13thc <71311541+hoanganh13thc@users.noreply.github.com>
2020-09-19 17:49:40 +0700

    Add files via upload

---
+++ lesson3.3.py
@@ -0,0 +1,6 @@
+name = "Hồ Vũ Hoàng Anh"
+MSSV = "1906020167"
+khoa = "Công Nghệ Thông Tin"
+lop = "13THC"
+chuyen_nganh = "Lập trình"
+print(f"Em là : {name} \n MSSV : {MSSV} \n Khoa : {khoa}\n LOP : {lop}\n Chuyên ngành : {chuyen_nganh}")
